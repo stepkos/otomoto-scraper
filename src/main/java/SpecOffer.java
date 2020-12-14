5c75222aee1b7e8d6822b8a9138e2a9b66e2b124
stepkos <52202262+stepkos@users.noreply.github.com>
2020-12-14 12:17:37 +0100

    Added parameters

--- src/main/java/SpecOffer.java
+++ src/main/java/SpecOffer.java
@@ -6,22 +6,29 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.function.Function;
 
 public class SpecOffer {
     public final String LINK;
     public final String TITLE;
     public final String SUBTITLE;
-//    public final String YEAR;
-//    public final String MILEAGE;
-//    public final String ENGINE_CAPACITY;
-//    public final String FUEL_TYPE;
+    public final String YEAR;
+    public final String MILEAGE;
+    public final String ENGINE_CAPACITY;
+    public final String FUEL_TYPE;
 //    public final String PRICE;
 //    public final String PRICE_CURRENCY;
 //    public final String LOCATION;
 
     public SpecOffer(Element offer) {
         Element offerContent = offer.getElementsByClass("offer-item__content").first();
-        Element title  = offerContent.getElementsByClass("offer-item__title").first();
+        Element title = offerContent.getElementsByClass("offer-item__title").first();
+
+        Elements parameters = offerContent
+                .getElementsByClass("ds-params-block")
+                .first()
+                .getElementsByClass("ds-param");
+
         String tmpSubtitle;
 
         try {
@@ -35,8 +42,26 @@ public class SpecOffer {
         TITLE = title.select("h2 a").first().text();
         SUBTITLE = tmpSubtitle;
 
+        if (parameters.size() < 4) {
+            YEAR = parameters.get(0).select("span").text();
+            ENGINE_CAPACITY = parameters.get(1).select("span").text();
+            FUEL_TYPE = parameters.get(2).select("span").text();
+            MILEAGE = "1 km";
+        }
+        else
+        {
+            YEAR = parameters.get(0).select("span").text();
+            MILEAGE = parameters.get(1).select("span").text();
+            ENGINE_CAPACITY = parameters.get(2).select("span").text();
+            FUEL_TYPE = parameters.get(3).select("span").text();
+        }
+
         System.out.println(TITLE);
         System.out.println(SUBTITLE);
+        System.out.println(YEAR);
+        System.out.println(MILEAGE);
+        System.out.println(ENGINE_CAPACITY);
+        System.out.println(FUEL_TYPE);
         System.out.println("------------");
     }
 
