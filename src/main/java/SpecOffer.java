8ed1e8cd0011d95cd551b33dae6a6c1141a37b89
stepkos <52202262+stepkos@users.noreply.github.com>
2020-12-14 11:26:12 +0100

    Done link, title and subtitle atributes

--- src/main/java/SpecOffer.java
+++ src/main/java/SpecOffer.java
@@ -8,14 +8,36 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class SpecOffer {
-//    public final Element BODY;
-//    public final String LINK;
+    public final String LINK;
+    public final String TITLE;
+    public final String SUBTITLE;
+//    public final String YEAR;
+//    public final String MILEAGE;
+//    public final String ENGINE_CAPACITY;
+//    public final String FUEL_TYPE;
+//    public final String PRICE;
+//    public final String PRICE_CURRENCY;
+//    public final String LOCATION;
 
-    public SpecOffer(Element element) {
-//        BODY = element;
-        System.out.println(element.getElementsByClass("ds-params-block").first().getElementsByClass("ds-param").get(1));
-//        Attributes attributes = element.getElementsByClass("offer__click-area").first().attributes();
-//        LINK = attributes.get("href");
+    public SpecOffer(Element offer) {
+        Element offerContent = offer.getElementsByClass("offer-item__content").first();
+        Element title  = offerContent.getElementsByClass("offer-item__title").first();
+        String tmpSubtitle;
+
+        try {
+            tmpSubtitle = title.select("h3").first().text();
+        }
+        catch (NullPointerException error) {
+            tmpSubtitle = "";
+        }
+
+        LINK = offer.attributes().get("data-href");
+        TITLE = title.select("h2 a").first().text();
+        SUBTITLE = tmpSubtitle;
+
+        System.out.println(TITLE);
+        System.out.println(SUBTITLE);
+        System.out.println("------------");
     }
 
 }
