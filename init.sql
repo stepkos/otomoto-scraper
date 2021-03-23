1c5cb5df61691814d643257deefebcced60e60b4
stepkos <stepkos@wp.pl>
2021-03-23 12:44:52 +0100

    init.sql

---
+++ init.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE otomoto;
+
+CREATE TABLE cars (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title varchar(100),
+    subtitle varchar(100),
+    year varchar(100),
+    mileage varchar(100),
+    engine_capacity varchar(100),
+    price varchar(100),
+    fuel_type varchar(100),
+    price_currency varchar(100),
+    location varchar(100),
+    link varchar(100)
+);
