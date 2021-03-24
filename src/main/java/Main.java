1623660827122b683843de2a69736400f8ffe49b
stepkos <stepkos@wp.pl>
2021-03-24 12:46:53 +0100

    finished functional version

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import java.sql.SQLException;
-
-public class Main {
-    public static void main(String[] args) throws SQLException {
-
-        new OtomotoScraperToDB("osobowe/opel/insignia/", true, true);
-
-    }
-}
--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,4 +1,6 @@
 import java.sql.*;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Scanner;
 
 public class Menu {
@@ -6,15 +8,51 @@ public class Menu {
 
     public static void main(String[] args) throws SQLException {
 
-        // LOGOWAONIE -> WYBÓR -> LINK -> WYJSCIE
+        String url;
+        Instant start = Instant.now();
+        Instant end = Instant.now();
+
         if (loginMenu()) {
             switch (displayMenu()) {
                 case 1:
-                    System.out.println("Wybrano 1");
+                    System.out.println("Wklej reszte linku bez 'otomoto.pl/': ");
+                    url = new Scanner(System.in).nextLine();
+                    start = Instant.now();
+                    new OtomotoScraper(url, true, "result/result.xls");
+                    end = Instant.now();
                     break;
+
                 case 2:
-                    System.out.println("Wybrano 2");
+                    System.out.println("Wklej reszte linku bez 'otomoto.pl/': ");
+                    url = new Scanner(System.in).nextLine();
+                    start = Instant.now();
+                    new OtomotoScraperToDB(url, true, true);
+                    end = Instant.now();
                     break;
+
+                case 3:
+                    System.out.println("Wklej reszte linku bez 'otomoto.pl/': ");
+                    url = new Scanner(System.in).nextLine();
+                    start = Instant.now();
+                    OtomotoScraper scraper = new OtomotoScraperToDB(url, true);
+                    end = Instant.now();
+                    for (SpecOffer offer : scraper.offers) {
+                        System.out.println(offer);
+                    }
+                    break;
+
+                case 4:
+                    start = Instant.now();
+                    ResultSet resultSet = getCarsFromDB();
+                    end = Instant.now();
+                    while (resultSet.next()) {
+                        for (int i=1; i<=12; i++) {
+                            System.out.println(resultSet.getString(i));
+                        }
+                        System.out.println("==================================================================");
+                    }
+                    break;
+
                 default:
                     System.out.println("Taka opcja nie instnieje");
             }
@@ -22,6 +60,8 @@ public class Menu {
         else {
             System.out.println("Niepoprawne dane logowania - Odmowa dostepu");
         }
+
+        System.out.println("Czas wykonania operacji: " + Duration.between(start, end).toMillis() + " millisekund/y");
 
     }
 
@@ -49,7 +89,9 @@ public class Menu {
 
 
         System.out.println("1. Zapisz do xls");
-        System.out.println("2. Zapisz do SQL");
+        System.out.println("2. Zapisz do bazy danych");
+        System.out.println("3. Wyswietl w konsoli");
+        System.out.println("4. Wyswietl zawartosc bazy danych");
 
         return new Scanner(System.in).nextInt();
     }
@@ -68,6 +110,15 @@ public class Menu {
                     return true;
         }
         return false;
+    }
+
+    public static ResultSet getCarsFromDB() throws SQLException {
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost/otomoto",
+                "root",
+                ""
+        );
+        return conn.createStatement().executeQuery("SELECT * FROM cars");
     }
 
 }
